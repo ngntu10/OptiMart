31f2e396f09c300dee4d2652f8e89a5a023bfe03
ngntu10 <phamnguyentu04@gmail.com>
2024-07-25 07:54:36 +0700

    initial setup

---
+++ src/main/java/com/Optimart/OptiMartApplication.java
@@ -0,0 +1,13 @@
+package com.Optimart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptiMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptiMartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Optimart/OptiMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Optimart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptiMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
