fc9972a55e5b81747f7ba756c0a5e7cfe5000be0
ngntu10 <phamnguyentu04@gmail.com>
2024-08-04 21:29:28 +0700

    feat: configure mapper model

---
+++ src/main/java/com/Optimart/config/MapperConfiguration.java
@@ -0,0 +1,12 @@
+package com.Optimart.config;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfiguration {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
