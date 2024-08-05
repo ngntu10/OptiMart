41106cd67f576d101648ee0ada38b557973ed417
ngntu10 <phamnguyentu04@gmail.com>
2024-08-05 12:59:42 +0700

    feat: update JWT token util

---
+++ src/main/java/com/Optimart/utils/JwtTokenUtil.java
@@ -0,0 +1,55 @@
+package com.Optimart.utils;
+
+import com.Optimart.exceptions.InvalidParamException;
+import com.Optimart.models.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtTokenUtil {
+    @Value("${jwt.expiration}")
+    private int expiration;
+
+    @Value("${jwt.secretKey}")
+    private String secretKey;
+
+    public String generateToken(User user) throws Exception{
+        Map<String,Object> claims =new HashMap<>();
+        claims.put("email", user.getEmail());
+        try {
+            String token = Jwts.builder()
+                    .setClaims(claims)
+                    .setSubject(user.getEmail())
+                    .setExpiration(new Date(System.currentTimeMillis()+expiration*1000L))
+                    .signWith(getSignInKey(), SignatureAlgorithm.ES256)
+                    .compact();
+            return token;
+        }catch (Exception ex){
+            throw new InvalidParamException("Cannot create jwt token, error: "+ex.getMessage());
+        }
+    }
+
+    private Key getSignInKey() {
+        byte[] bytes = Decoders.BASE64.decode(secretKey);
+        //Keys.hmacShaKeyFor(Decoders.BASE64.decode("TaqlmGv1iEDMRiFp/pHuID1+T84IABfuA0xXh4GhiUI="));
+        return Keys.hmacShaKeyFor(bytes);
+    }
+
+    //    private String generateSecretKey() {
+//        SecureRandom random = new SecureRandom();
+//        byte[] keyBytes = new byte[32]; // 256-bit key
+//        random.nextBytes(keyBytes);
+//        String secretKey = Encoders.BASE64.encode(keyBytes);
+//        return secretKey;
+//    }
+
+}
